d538b95be6f37835819066aec94b33533d24ca08
matheuspaz123 <matheuspaz30071998@gmail.com>
2020-05-29 00:12:37 -0300

    criada intents para cada Button da LoginActivity

---
+++ app/src/main/java/com/example/cafeteria_001/view/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cafeteria_001.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cafeteria_001.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
