d81cb60e878d655bcd89634c4cf6cdf497eb3180
matheuspaz123 <matheuspaz30071998@gmail.com>
2020-05-29 16:56:03 -0300

    Botões customizados

--- app/src/main/java/com/example/cafeteria_001/view/SignInActivity.kt
+++ app/src/main/java/com/example/cafeteria_001/view/SignInActivity.kt
@@ -8,6 +8,6 @@ class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)teste
+        setContentView(R.layout.activity_sign_in)
     }
 }
