bc68e604119aed437c72311e4e7ba9cffd482b9c
matheuspaz123 <matheuspaz30071998@gmail.com>
2020-06-04 02:43:06 -0300

    Comunicação Api de CEP feita e binding de dados no layout completo

--- app/src/main/java/com/example/cafeteria_001/view/SignUpActivity.kt
+++ app/src/main/java/com/example/cafeteria_001/view/SignUpActivity.kt
@@ -3,44 +3,54 @@ package com.example.cafeteria_001.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.cafeteria_001.R
 import com.example.cafeteria_001.model.remote.CepModel
 import com.example.cafeteria_001.model.remote.CepRetrofit
 import com.example.cafeteria_001.model.remote.CepService
+import com.example.cafeteria_001.viewmodel.SignUpViewModel
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class SignUpActivity : AppCompatActivity() {
+
+    var call: Call<CepModel>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        button_sign_up.setOnClickListener {
-            var cep = text_sign_up_cep.text.toString()
+        val mViewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
 
-            if (cep != "") {
-                //Instanciei o retrofit com a interface passada como referencia
-                val cepRetrofit = CepRetrofit.createServiceCep(CepService::class.java)
-                var call: Call<CepModel> = cepRetrofit.getCEP(cep)
-
-                val response = call.enqueue(object : Callback<CepModel>{
-                    override fun onFailure(call: Call<CepModel>, t: Throwable) {
-                        //não conseguiu comunicar
-                        Toast.makeText(applicationContext, "$t", Toast.LENGTH_LONG).show()
-                    }
-
-                    override fun onResponse(call: Call<CepModel>, response: Response<CepModel>) {
-                        val teste = response.body()
-                        Toast.makeText(applicationContext, "$response", Toast.LENGTH_LONG).show()
-                    }
-
-                })
-            }
-
+        button_sign_up_atualization.setOnClickListener {
+            bindCep()
         }
-
     }
+
+    fun bindCep() {
+        val cep = text_sign_up_cep.text.toString()
+        if (cep.length == 8) {
+            val cepRetrofit = CepRetrofit.createServiceCep(CepService::class.java)
+            var call: Call<CepModel> = cepRetrofit.getCEP(cep)
+
+            val response = call.enqueue(object : Callback<CepModel> {
+                override fun onFailure(call: Call<CepModel>, t: Throwable) {
+
+                }
+
+                override fun onResponse(call: Call<CepModel>, response: Response<CepModel>) {
+                    val aux = response.body()
+                    text_view_sign_up_localidade.text = aux?.localidade
+                    text_view_sign_up_uf.text = aux?.uf
+                    text_view_sign_up_logradouro.text = aux?.logradouro
+                }
+            })
+        }else{
+            Toast.makeText(applicationContext,"Cep invalido", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
