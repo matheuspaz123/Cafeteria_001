04cd13469735db375301a96b82f9e0a6f785532e
matheuspaz123 <matheuspaz30071998@gmail.com>
2020-06-10 01:33:09 -0300

    criando activity menu de produtos

---
+++ app/src/main/java/com/example/cafeteria_001/view/ProductMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cafeteria_001.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cafeteria_001.R
+
+class ProductMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_menu)
+    }
+}
