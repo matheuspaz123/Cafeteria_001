463657066e88c0a1ace715f148db1fe2d3931bdb
matheuspaz123 <matheuspaz30071998@gmail.com>
2020-06-11 17:17:35 -0300

    finalizada lista de produtos

--- app/src/main/java/com/example/cafeteria_001/view/SignInActivity.kt
+++ app/src/main/java/com/example/cafeteria_001/view/SignInActivity.kt
@@ -1,13 +1,19 @@
 package com.example.cafeteria_001.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cafeteria_001.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        button_login_local.setOnClickListener {
+            startActivity(Intent(applicationContext, ProductMenuActivity::class.java))
+        }
     }
 }
