678ac931f379f13c0090d5bfd20d7bf819c2df41
Melker Halberd <melker168@gmail.com>
2020-08-06 13:59:38 -0300

    só testando o git agora pode recusar esse commit

---
+++ app/src/main/java/com/example/cafeteria_001/teste.kt
@@ -0,0 +1,11 @@
+package com.example.cafeteria_001
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class teste : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teste)
+    }
+}
